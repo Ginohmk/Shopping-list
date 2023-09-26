47630d2dfd173f24e6c2d94563813c2f470ac213
ginohmk <michotall95@gmail.com>
2023-09-26 11:23:00 +0100

    feat: Add user model for creating user and super user

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+#  Specify Where user model is located
+
+AUTH_USER_MODEL = "user.User"
