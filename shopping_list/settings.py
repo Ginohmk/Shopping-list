f52e0377521018d25761b0d06a075eeddc005ec5
ginohmk <michotall95@gmail.com>
2023-09-28 12:55:01 +0100

    feat: changr Debug to False

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!1+!4_^3p80r=)ca4$$nt9np#ck=$qjpud#30mkv#&vz%8p_dx"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 USER_ENVIRONMENT = "http://127.0.0.1:8000" if DEBUG else "Live"
