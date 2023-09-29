91b184c90c88322b04985daa6666340a8ed1a5f0
ginohmk <michotall95@gmail.com>
2023-09-29 17:32:28 +0100

    feat: add comment doc string

--- shop/services.py
+++ shop/services.py
@@ -31,6 +31,30 @@ def update_delete_retreive_shop_checker(shop_instance, user_instance):
 
 @dataclasses.dataclass
 class ShopDataClass:
+    """Defines the data struture of the user for response.
+
+    Class varibles
+    ----------
+    title : str
+        The user first name
+
+    priority : str
+        The user last name
+
+    due_date : str
+        The user email
+
+
+    description : str, default None
+        The user password
+
+
+    Methods
+    ------
+    from_instance(cls, user)
+        Returns object of class
+    """
+
     title: str
     priority: int
     due_date: datetime.datetime
@@ -57,6 +81,21 @@ class ShopDataClass:
 
 
 def create_shop(user: "User", shop: "Shop") -> "ShopDataClass":
+    """Create a shop saved to db
+
+    Parameters
+    ----------
+    user : str
+        user objects that needs to be saved in database
+
+    shop_id : str
+        Shop id to be updated
+
+    Returns
+    -------
+      ShopDataClass object variables
+    """
+
     shop = models.Shop(
         title=shop.title,
         priority=shop.priority,
@@ -74,12 +113,42 @@ def create_shop(user: "User", shop: "Shop") -> "ShopDataClass":
 
 
 def get_user_shop_list(user_details: "User") -> list["ShopDataClass"]:
+    """Get all shops owened by user
+
+    Parameters
+    ----------
+    user : str
+        user objects that needs to be saved in database
+
+    shop_id : str
+        Shop id to be updated
+
+    Returns
+    -------
+      list of ShopDataClass object variables
+    """
+
     shop_list = models.Shop.objects.filter(user=user_details)
 
     return [ShopDataClass.from_instance(single_shop) for single_shop in shop_list]
 
 
 def get_user_shop_item(user: "User", shop_id: int) -> "ShopDataClass":
+    """Get and display a  shop by id
+
+    Parameters
+    ----------
+    user : str
+        user objects that needs to be saved in database
+
+    shop_id : str
+        Shop id to be updated
+
+    Returns
+    -------
+      ShopDataClass object variables
+    """
+
     check_valid_uuid(shop_id)
 
     shop = models.Shop.objects.filter(id=shop_id).first()
@@ -90,6 +159,21 @@ def get_user_shop_item(user: "User", shop_id: int) -> "ShopDataClass":
 
 
 def delete_user_shop_item(user: "User", shop_id: str):
+    """Delete shop by id
+
+    Parameters
+    ----------
+    user : str
+        user objects that needs to be saved in database
+
+    shop_id : str
+        Shop id to be updated
+
+    Returns
+    -------
+      ShopDataClass object variables
+    """
+
     check_valid_uuid(shop_id)
 
     shop = models.Shop.objects.filter(id=shop_id).first()
@@ -102,6 +186,24 @@ def delete_user_shop_item(user: "User", shop_id: str):
 def update_user_shop_item(
     user: "User", shop_id: str, shop_data: "Shop"
 ) -> "ShopDataClass":
+    """Update shop by  user to the database.
+
+    Parameters
+    ----------
+    user : str
+        user objects that needs to be saved in database
+
+    shop_id : str
+        Shop id to be updated
+
+    shop_data : str
+        Shop data contains efit information id
+
+
+    Returns
+    -------
+      ShopDataClass object variables
+    """
     check_valid_uuid(shop_id)
 
     shop = models.Shop.objects.filter(id=shop_id).first()
