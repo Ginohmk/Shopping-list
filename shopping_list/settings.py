02abf2773c894a67ba921e61fd02b8f40c1908bd
ginohmk <michotall95@gmail.com>
2023-09-29 09:45:50 +0100

    feat: change dn path

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -108,9 +108,11 @@ WSGI_APPLICATION = "shopping_list.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+
+# "NAME": BASE_DIR / "db.sqlite3",
 
 
 # Password validation
